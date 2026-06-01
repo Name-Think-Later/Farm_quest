b863625e544daf4621dc0d1d4b1c235607b7a94f
ukaba05 <ukaba05@gmail.com>
2026-06-01 20:07:56 +0800

    chore: setup environment variable configuration - Install environment variable loader package - Add .env to .gitignore

--- src/main/java/nutc/sot/farm_quest/FarmQuestApplication.java
+++ src/main/java/nutc/sot/farm_quest/FarmQuestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FarmQuestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FarmQuestApplication.class, args);
     }
