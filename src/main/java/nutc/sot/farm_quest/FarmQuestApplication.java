a863877c0e1b1c2a134b627425cae8f3ef5e38f5
ukaba05 <ukaba05@gmail.com>
2026-06-01 16:33:26 +0800

    Init

---
+++ src/main/java/nutc/sot/farm_quest/FarmQuestApplication.java
@@ -0,0 +1,13 @@
+package nutc.sot.farm_quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmQuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nutc/sot/farm_quest/FarmQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package nutc.sot.farm_quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
